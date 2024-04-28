fc39df410e13dace94ef4363b8e52055cf8f2929
Axelandrovitch <axel.a.hetru@gmail.com>
2024-04-28 16:18:13 +0200

    add unit test, refactor TextNode class

--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,13 @@
 class TextNode:
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type = "text", url = None):
         self.text = text
         self.text_type = text_type
         self.url = url
     def __eq__(self, other):
-        self.text == other.text
-        self.text_type == other.text_type
-        self.url == other.url
+        if not isinstance(other, TextNode):
+            return False
+
+        return (self.text == other.text and self.text_type == other.text_type and self.url == other.url)
     def __repr__(self):
         return f"{self.text}, {self.text_type}, {self.url}"
 
