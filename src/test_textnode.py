53a6f0379f044bcc7338660d6e012bca0f455930
Axelandrovitch <axel.a.hetru@gmail.com>
2024-04-28 17:20:03 +0200

    refactor unit test and add test to compare with other class instance

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -6,24 +6,36 @@ from textnode import TextNode
 class TestTextNode(unittest.TestCase):
     
     def setUp(self):
+        # Full node arbitrary sample
         self.full_node = TextNode("This is a text node", "bold", "https://boot.dev")
-        self.full_node_duplicate = TextNode("This is a text node", "bold", "https://boot.dev")
-        self.same_text_default_type_text_default_url = TextNode("This is a text node", "text", None)
-        self.same_text_default_type_no_url = TextNode("This is a text node", "text")
+
         self.full_node_different_url = TextNode("This is a text node", "bold", "https://not-the-same.dev")
         self.text_and_type_text_node = TextNode("This is a text node", "bold")
         self.text_and_type_text_node_duplicate = TextNode("This is a text node", "bold")
-        self.different_text_and_same_type_text_node =  TextNode("This is another a text node", "bold")
+
         self.same_text_and_different_type_text_node =  TextNode("This is a text node", "italic")
+
         self.only_text_node = TextNode("This is a text node")
         self.only_text_different_node = TextNode("This is another text node")
 
+        # Note that the default node differs from the full test node
+        self.same_text_default_node= TextNode("This is a text node", "text", None)
+
+        
+
 
     def test_eq(self):
-        self.assertEqual(self.full_node, self.full_node_duplicate)
-    
+        full_node_duplicate = TextNode("This is a text node", "bold", "https://boot.dev")
+        self.assertEqual(self.full_node, full_node_duplicate)
+    def test_not_same_instance(self):
+        class OtherClass:
+            def __init__(self, other):
+                self.other = other
+        
+        other_class_instance = OtherClass("other")
+        self.assertFalse(self.full_node == other_class_instance) 
     def test_text_type_default(self):
-        self.assertEqual(self.only_text_node, self.same_text_default_type_no_url)
+        self.assertEqual(self.only_text_node, self.same_text_default_node)
     
     def test_inequality_due_to_different_text(self):
         self.assertNotEqual(self.only_text_node, self.only_text_different_node)
@@ -31,7 +43,9 @@ class TestTextNode(unittest.TestCase):
     def test_inequality_due_to_different_text_type(self):
         self.assertNotEqual(self.text_and_type_text_node, self.same_text_and_different_type_text_node)
     def test_inequality_due_to_different_url(self):
+        node_empty_url = TextNode("This is a text node", "bold", "")
         self.assertNotEqual(self.full_node, self.full_node_different_url)
+        self.assertNotEqual(self.full_node, node_empty_url)
 
 
 
