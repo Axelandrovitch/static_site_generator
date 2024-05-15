9ed76485216927188eb888a66364336deca0879d
Axelandrovitch <axel.a.hetru@gmail.com>
2024-05-15 23:39:52 +0200

    refined split_node_delimeter function by handling case with bold in italic

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,85 +1,84 @@
 class HTMLNode:
-  def __init__(self, tag = None, value = None, children = None, props = None):
-    self.tag = tag
-    self.value = value
-    self.children = children
-    self.props = props
-  
-  def to_html(self):
-    raise NotImplementedError
-  
-  def props_to_html(self):
-    if self.props is None:
-      return ""
-    else:
-      props_to_concatenate = []
-      for key, value in self.props.items():
-        props_to_concatenate.append(f'{key}="{value}"')
-      return " ".join(props_to_concatenate)
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
 
+    def to_html(self):
+        raise NotImplementedError
 
-  def __repr__(self):
-    props_str = ""
-    if self.props is not None:
-      props_str = self.props_to_html()
-    def check_props(props):
-      if len(props) > 0 and type(props) == str:
-        return f" props = {props},"
-      return ""
-    children_reprs = [repr(child) for child in self.children] if self.children else "no children"
-    return f"tag = {self.tag}, value = {self.value},{check_props(props_str)} children = {children_reprs}"
-  
-  def text_node_to_html_node(self, text_node):
-    text = text_node.text
-    url = text_node.url
-    match text_node.text_type:
-      case "text":
-        return LeafNode(tag=None, value=text)
-      case "bold":
-        return LeafNode(tag="b", value=text)
-      case "italic":
-        return LeafNode(tag="i", value=text)
-      case "code":
-        return LeafNode(tag="code", value=text)
-      case "link":
-        return LeafNode(tag="a", value=text, props={"href": url})
-      case "image":
-        return LeafNode(tag="img", value="", props={"src": url, "alt": text})
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        else:
+            props_to_concatenate = []
+            for key, value in self.props.items():
+                props_to_concatenate.append(f'{key}="{value}"')
+            return " ".join(props_to_concatenate)
 
+    def __repr__(self):
+        props_str = ""
+        if self.props is not None:
+            props_str = self.props_to_html()
+
+        def check_props(props):
+            if len(props) > 0 and isinstance(props, str):
+                return f" props = {props},"
+            return ""
+
+        children_reprs = [repr(child) for child in self.children] if self.children else "no children"
+        return f"tag = {self.tag}, value = {self.value},{check_props(props_str)} children = {children_reprs}"
+
+    def text_node_to_html_node(self, text_node):
+        text = text_node.text
+        url = text_node.url
+        match text_node.text_type:
+            case "text":
+                return LeafNode(tag=None, value=text)
+            case "bold":
+                return LeafNode(tag="b", value=text)
+            case "italic":
+                return LeafNode(tag="i", value=text)
+            case "code":
+                return LeafNode(tag="code", value=text)
+            case "link":
+                return LeafNode(tag="a", value=text, props={"href": url})
+            case "image":
+                return LeafNode(tag="img", value="", props={"src": url, "alt": text})
 
 
 class LeafNode(HTMLNode):
-  def __init__(self, tag, value, props = None):
-    super().__init__(tag, value, None, props)
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
 
-  def __eq__(self, other):
-    if not isinstance(other, LeafNode):
-      return NotImplemented
-    return (self.tag == other.tag and self.value == other.value and self.props == other.props)
-  
-  def to_html(self):
-    if self.value is None or self.value =="":
-      raise ValueError("This node must have a value")
-    elif self.tag is None:
-      return str(self.value)
-    else:
-      props_str =" " + self.props_to_html() if self.props_to_html() != "" else ""
-      return f"<{self.tag}{props_str}>{self.value}</{self.tag}>"
+    def __eq__(self, other):
+        if not isinstance(other, LeafNode):
+            return NotImplemented
+        return self.tag == other.tag and self.value == other.value and self.props == other.props
+
+    def to_html(self):
+        if self.value is None or self.value == "":
+            raise ValueError("This node must have a value")
+        elif self.tag is None:
+            return str(self.value)
+        else:
+            props_str = " " + self.props_to_html() if self.props_to_html() != "" else ""
+            return f"<{self.tag}{props_str}>{self.value}</{self.tag}>"
 
 
 class ParentNode(HTMLNode):
-  def __init__(self, tag, children, props = None):
-    super().__init__(tag, None, children, props)
-  
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
 
-  def to_html(self):
-    if self.tag is None or self.tag == "":
-      raise ValueError("A tag must be provided for a parent node")
-    if self.children is None or len(self.children) == 0:
-      raise ValueError("A parent node must have at least one child")
-    for children in self.children:
-      if not isinstance(children, ParentNode) and not isinstance(children, LeafNode):
-        raise TypeError("The children must be of type LeafNode or ParentNode")
-    else:
-      concat_children = "".join([child.to_html() for child in self.children])
-      return f"<{self.tag}>{concat_children}</{self.tag}>"
+    def to_html(self):
+        if self.tag is None or self.tag == "":
+            raise ValueError("A tag must be provided for a parent node")
+        if self.children is None or len(self.children) == 0:
+            raise ValueError("A parent node must have at least one child")
+        for children in self.children:
+            if not isinstance(children, ParentNode) and not isinstance(children, LeafNode):
+                raise TypeError("The children must be of type LeafNode or ParentNode")
+        else:
+            concat_children = "".join([child.to_html() for child in self.children])
+            return f"<{self.tag}>{concat_children}</{self.tag}>"
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,9 @@
 import random
 import string
+
+
 class TextNode:
-    def __init__(self, text, text_type = "text", url = None):
+    def __init__(self, text, text_type="text", url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -9,69 +11,86 @@ class TextNode:
     def __eq__(self, other):
         if not isinstance(other, TextNode):
             return False
-        return (self.text == other.text and self.text_type == other.text_type and self.url == other.url)
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
 
     def __repr__(self):
         return f"{self.text}, {self.text_type}, {self.url}"
-    
 
 
-def split_nodes_delimeter(old_nodes, delimeter, text_type):
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
     all_nodes = []
     for node in old_nodes:
         if not isinstance(node, TextNode):
             raise TypeError(f"Node: {node} is not a TextNode")
-        splitted_node = manage_delimeter(delimeter, node.text)
+        split_node = manage_delimiter(delimiter, node.text)
         converted_node = []
-        for i in range(0, len(splitted_node)):
-            raw_text = splitted_node[i]
+        for i in range(0, len(split_node)):
+            raw_text = split_node[i]
             if i % 2 != 0:
                 converted_node.append(TextNode(text=raw_text, text_type=text_type))
             elif raw_text != "":
                 converted_node.append(TextNode(text=raw_text, text_type="text"))
         all_nodes.extend(converted_node)
     return all_nodes
-  
 
 
-def manage_delimeter(delimeter, text):
-  if delimeter == "*" and "**" in text:
-      random_tag = "_"
-      while random_tag in text:
-          random_tag = generate_random_tag()
-      text = text.replace("**", random_tag)
-      splitted_text = text.split(delimeter)
-      splitted_text = [i.replace(random_tag, "**") if random_tag in i else i for i in splitted_text]
-      return splitted_text
-  else:
-      return text.split(delimeter) 
+def manage_delimiter(delimiter, text):
+    if text.count(delimiter) % 2 != 0:
+        raise ValueError("Opened delimiters must be closed")
+    elif delimiter == "*" and "**" in text:
+        nodes = []
+        italic = False
+        current_text = ""
+        asterisk_occurrence = 0
+        for i in range(len(text)):
+            next_i = text[i + 1] if i < len(text) - 1 else None
+
+            if text[i] == "*":
+                asterisk_occurrence += 1
+
+                if asterisk_occurrence == 1 and next_i != "*":
+                    italic = not italic
+                    if italic and current_text:
+                        nodes.append(TextNode(text=current_text, text_type="text"))
+                        current_text = ""
+                    elif not italic and current_text:
+                        nodes.append(TextNode(text=current_text, text_type="italic"))
+                        current_text = ""
+
+                if asterisk_occurrence == 2:
+                    current_text += "**"
+                    asterisk_occurrence = 0
+
+            else:
+                current_text += text[i]
+                asterisk_occurrence = 0
+
+            if i == len(text) - 1 and current_text:
+                if italic:
+                    nodes.append(TextNode(text=current_text, text_type="italic"))
+                else:
+                    nodes.append(TextNode(text=current_text, text_type="text"))
+
+        return nodes
+    else:
+        return text.split(delimiter)
+
 
 def generate_random_tag():
-  return ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(10))
-
-  def append_node(splited_node, converted_node, text_type):
-    for i in range(0, len(splited_node)):
-        raw_text = splited_node[i]
-        if i % 2 != 0:
-            converted_node.append(TextNode(text=raw_text, text_type=text_type))
-        elif raw_text != "":
-            converted_node.append(TextNode(text=raw_text, text_type="text"))
-    return converted_node
-
-                
+    return ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(10))
 
 
-text_node_text = TextNode(text="Hello World")
-text_node_bold= TextNode(text="**bold text** not bold")
-text_node_italic = TextNode(text="not italic*italic text*")
-text_node_code = TextNode(text="some other stuff `code text` not code")
+text_node_text = TextNode(text="*Hello**World**this is a test*")
+text_node_bold = TextNode(text="bold t*ext not* bold")
+text_node_italic = TextNode(text="not *italic* italic* text*")
+text_node_code = TextNode(text="some other stuff `code text not code")
 
 text_node_1 = TextNode(text="Hello World", text_type="text")
-text_node_2 = TextNode(text="**bold text** not bold", text_type="text")
+text_node_2 = TextNode(text="bold text not bold", text_type="text")
 text_node_3 = TextNode(text="not italic", text_type="text")
 text_node_4 = TextNode(text="italic text", text_type="italic")
 text_node_5 = TextNode(text="some other stuff `code text` not code", text_type="text")
 expected_italic = [text_node_1, text_node_2, text_node_3, text_node_4, text_node_5]
 
-test1 = split_nodes_delimeter([text_node_text, text_node_bold, text_node_italic, text_node_code], "*", "italic")
+test1 = split_nodes_delimiter([text_node_text, text_node_bold, text_node_italic, text_node_code], "*", "italic")
 print(test1)
