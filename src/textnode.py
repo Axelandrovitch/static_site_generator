809df87b7980b307a9210a960c5792a3320ed0c5
Axelandrovitch <axel.a.hetru@gmail.com>
2024-05-24 14:11:42 +0200

    update split_nodes_delimiter function / does not support nested italic and bold: inner bold or inner italic will not be considered. Possible TODO. Add treat_markdown file with split_node_image and split_node_links functions

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,9 @@
-import random
-import string
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
 
 
 class TextNode:
@@ -18,79 +22,32 @@ class TextNode:
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    all_nodes = []
-    for node in old_nodes:
-        if not isinstance(node, TextNode):
-            raise TypeError(f"Node: {node} is not a TextNode")
-        split_node = manage_delimiter(delimiter, node.text)
-        converted_node = []
-        for i in range(0, len(split_node)):
-            raw_text = split_node[i]
-            if i % 2 != 0:
-                converted_node.append(TextNode(text=raw_text, text_type=text_type))
-            elif raw_text != "":
-                converted_node.append(TextNode(text=raw_text, text_type="text"))
-        all_nodes.extend(converted_node)
-    return all_nodes
-
-
-def manage_delimiter(delimiter, text):
-    if text.count(delimiter) % 2 != 0:
-        raise ValueError("Opened delimiters must be closed")
-    elif delimiter == "*" and "**" in text:
-        nodes = []
-        italic = False
-        current_text = ""
-        asterisk_occurrence = 0
-        for i in range(len(text)):
-            next_i = text[i + 1] if i < len(text) - 1 else None
-
-            if text[i] == "*":
-                asterisk_occurrence += 1
-
-                if asterisk_occurrence == 1 and next_i != "*":
-                    italic = not italic
-                    if italic and current_text:
-                        nodes.append(TextNode(text=current_text, text_type="text"))
-                        current_text = ""
-                    elif not italic and current_text:
-                        nodes.append(TextNode(text=current_text, text_type="italic"))
-                        current_text = ""
-
-                if asterisk_occurrence == 2:
-                    current_text += "**"
-                    asterisk_occurrence = 0
-
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != text_type_text:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("Invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], text_type_text))
             else:
-                current_text += text[i]
-                asterisk_occurrence = 0
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
 
-            if i == len(text) - 1 and current_text:
-                if italic:
-                    nodes.append(TextNode(text=current_text, text_type="italic"))
-                else:
-                    nodes.append(TextNode(text=current_text, text_type="text"))
-
-        return nodes
-    else:
-        return text.split(delimiter)
-
-
-def generate_random_tag():
-    return ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(10))
-
-
-text_node_text = TextNode(text="*Hello**World**this is a test*")
-text_node_bold = TextNode(text="bold t*ext not* bold")
-text_node_italic = TextNode(text="not *italic* italic* text*")
-text_node_code = TextNode(text="some other stuff `code text not code")
 
 text_node_1 = TextNode(text="Hello World", text_type="text")
-text_node_2 = TextNode(text="bold text not bold", text_type="text")
-text_node_3 = TextNode(text="not italic", text_type="text")
-text_node_4 = TextNode(text="italic text", text_type="italic")
-text_node_5 = TextNode(text="some other stuff `code text` not code", text_type="text")
-expected_italic = [text_node_1, text_node_2, text_node_3, text_node_4, text_node_5]
+text_node_2 = TextNode(text="**this is why** *I never go* to this place !", text_type="text")
+text_node_3 = TextNode(text="it is such an idiotic thing *to do*", text_type="text")
+text_node_4 = TextNode(text="Whatever", text_type="text")
+text_node_5 = TextNode(text="I am going **home** to write some `code text`", text_type="text")
 
-test1 = split_nodes_delimiter([text_node_text, text_node_bold, text_node_italic, text_node_code], "*", "italic")
-print(test1)
+test = split_nodes_delimiter([text_node_1, text_node_2, text_node_3, text_node_4, text_node_5], "*", "italic")
+for node in test:
+    print(node)
--- src/treat_markdown.py
+++ src/treat_markdown.py
@@ -1,16 +1,87 @@
 import re
-
+from textnode import TextNode
+from textnode import manage_delimiter, split_nodes_delimiter
 
 def extract_markdown_images(text):
     matches = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
     return matches
 
-text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
-print(extract_markdown_images(text))
+# text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
+# print(extract_markdown_images(text))
 
 def extract_markdown_links(text):
     matches = re.findall(r"\[(.*?)\]\((.*?)\)", text)
     return matches
 
-text2 = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
-print(extract_markdown_links(text2))
+# text2 = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
+# print(extract_markdown_links(text2))
+
+
+def split_node_image(old_nodes):
+    nodes = []
+    for node in old_nodes:
+        original_text = node.text
+        extracted_images = extract_markdown_images(original_text)
+        if not extracted_images:  # If no images found, add the original node back
+          nodes.append(node)
+          continue
+        for image in extracted_images:
+          image_label = image[0]
+          image_url = image[1]
+          split_text = original_text.split(f"![{image_label}]({image_url})", 1)
+          nodes.append(TextNode(text=split_text[0], text_type="text"))
+          if split_text[0]:
+            nodes.append(TextNode(text_type="img", text=image[0], url=image[1]))
+            original_text = split_text[1]
+        if original_text:
+           nodes.append(TextNode(text=original_text, text_type="text"))
+    return nodes
+
+def split_node_link(old_nodes):
+    nodes = []
+    for node in old_nodes:
+        original_text = node.text
+        extracted_images = extract_markdown_links(original_text)
+        if not extracted_images:  
+          nodes.append(node)
+          continue
+        for image in extracted_images:
+          image_label = image[0]
+          image_url = image[1]
+          split_text = original_text.split(f"![{image_label}]({image_url})", 1)
+          nodes.append(TextNode(text=split_text[0], text_type="text"))
+          if split_text[0]:
+            nodes.append(TextNode(text_type="link", text=image[0], url=image[1]))
+            original_text = split_text[1]
+        if original_text:
+           nodes.append(TextNode(text=original_text, text_type="text"))
+    return nodes
+
+def text_to_text_nodes(text):
+   result = split_nodes_delimiter(text)
+   
+
+node = TextNode(
+    "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
+    "text",
+)
+
+new_nodes = split_node_image([node])
+
+print(new_nodes)
+
+
+node2 = TextNode(
+    "This is text with an ![image1](https://example.com/image1.png) and another ![image2](https://example.com/image2.png) here.",
+    text_type="text"
+)
+new_nodes2 = split_node_image([node2])
+# print(new_nodes2)
+
+node_links = TextNode(
+    "Here is a [link1](https://example.com/link1) and here is another [link2](https://example.com/link2) and some more text.",
+    text_type="text"
+)
+new_nodes_link = split_node_link([node])
+
+print(new_nodes_link)
