519bcd03ecb69225d14c8b9f57d9bf3ec6b3cfcd
Axelandrovitch <axel.a.hetru@gmail.com>
2024-05-26 18:54:29 +0200

    add generate_page function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
-from textnode import TextNode
+import os
 from copystatic import copy_directory_contents
+from generatepage import generate_page
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
+
 
 def main():
 
   copy_directory_contents(src="static", dst="public")
+  generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html"),
+    )
+
 
 main()
